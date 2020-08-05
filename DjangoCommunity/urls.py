894d3d7ca9a6dbbe36f93eb3e75388a76a409065
kbsat <kbsat@naver.com>
2020-08-05 19:09:20 +0900

    update 기능 구현. CRUD 완성

--- DjangoCommunity/urls.py
+++ DjangoCommunity/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('new/', community.views.new, name="new"),
     path('detail/<int:post_id>', community.views.detail, name="detail"),
     path('delete/<int:post_id>', community.views.delete, name="delete"),
+    path('update/<int:post_id>', community.views.update, name="update"),
+    path('update_save/<int:post_id>',
+         community.views.update_save, name="update_save"),
 ]
--- community/views.py
+++ community/views.py
@@ -24,6 +24,23 @@ def detail(request, post_id):
     return render(request, 'detail.html', {'post': post})
 
 
+def update(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'update.html', {'post': post})
+
+
+def update_save(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    new_title = request.GET['title']
+    new_content = request.GET['content']
+    if post.title != new_title or post.content != new_content:
+        post.title = new_title
+        post.content = new_content
+        post.save()
+
+    return redirect('/detail/'+str(post_id))
+
+
 def new(request):
     post = Post()
     post.title = request.GET['title']
